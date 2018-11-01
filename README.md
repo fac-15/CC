1b3bbec7eaca56a91ab51ea296dd1afd83dace8e
Martin Bagshaw <martinbagshaw1@gmail.com>
2018-11-01 15:35:19 +0000

    form validation and styling

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Team Colour Code one page website
 
-- colour code colours:
-  - ![#ebebd3](https://placehold.it/15/ebebd3/000000?text=+) `#ebebd3`
-  - ![#ff9b71](https://placehold.it/15/ff9b71/000000?text=+) `#ff9b71`
-  - ![#49dcb1](https://placehold.it/15/49dcb1/000000?text=+) `#49dcb1`
-  - ![#046865](https://placehold.it/15/046865/000000?text=+) `#046865`
+**Colour Code Colours:**
+  ![#ebebd3](https://placehold.it/15/ebebd3/000000?text=+) `#ebebd3`
+  ![#ff9b71](https://placehold.it/15/ff9b71/000000?text=+) `#ff9b71`
+  ![#49dcb1](https://placehold.it/15/49dcb1/000000?text=+) `#49dcb1`
+  ![#046865](https://placehold.it/15/046865/000000?text=+) `#046865`
 
-- ARIA stuff:
- - [regional landmarks](https://www.w3.org/TR/wai-aria-practices/examples/landmarks/region.html).
+**ARIA stuff:**
+ [regional landmarks](https://www.w3.org/TR/wai-aria-practices/examples/landmarks/region.html).
+
+
+**Form Validation**
+ [email validation regex](https://www.w3resource.com/javascript/form/email-validation.php)
+ [more on MDN](https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms/Form_validation)
--- main.js
+++ main.js
@@ -4,27 +4,111 @@
 // var numberRegex = /[0-9]/
 
 
-function validator() {
-    var nameRegex = /[^0-9]$/;  
-    // console.log(e);
-    // var nameValue = ;
-
-    // console.log(nameValue);
 
 
-    if(nameRegex.test('keydown')){
-        console.log('valid');
-    } else {
-        console.log('invalid')
+/*
+Name
+    - letters a-z, case insensitive
+
+Company Name
+    - letters a-z AND numbers 0-9, case insensitive
+
+Email
+    - must have an @ sign
+
+Phone Number
+    - numbers 0-9, and 11 characters long
+
+Message
+    - greater than 200 characters
+    - escape html
+*/
+
+
+// if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value)
+
+
+
+function validator(e) {
+
+    // regexes
+    // const nameRegex = /[^0-9]$/;
+    // const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    // old email regex: [\w_.]+@[\w_.]{3,}\.{1}[a-z]{2,}$
+    // new email regex: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+    // error message
+    const errorP = e.target.nextElementSibling;
+    
+    // add or remove error classes
+    // - not valid:
+    if (!e.target.validity.valid) {
+        e.target.className = 'error';
+        errorP.className = 'error-msg-show';
     }
+    // valid
+    if (e.target.validity.valid) {
+        e.target.className = '';
+        errorP.innerHTML = '';
+    }
+
+
+    // error messages:
+    const nameMsg = 'Please enter a valid name. You can enter letters a-z, upper and lower case';
+    const companyMsg = 'Please enter a valid company name. You can enter letters a-z, upper and lower case';
+    const emailMsg = 'Please enter a valid email address.';
+    const phoneMsg = 'Your phone number is too long. Please enter up to 12 digits only.';
+
+
+    // target different input fields, if they are invalid:
+    if (!e.target.validity.valid) {
+        // - name
+        if (e.target.id == 'name') {
+            errorP.innerHTML = nameMsg;
+        }
+        // - company name
+        else if (e.target.id == 'companyName') {
+            errorP.innerHTML = companyMsg;
+        }
+        // - email
+        else if (e.target.type == 'email') {
+            errorP.innerHTML = emailMsg;
+        }
+        // - phone
+        else if (e.target.type == 'number' || e.target.id == 'number') {
+            if (e.target.value.length>12) {
+                errorP.innerHTML = phoneMsg;
+            }
+        }
+        // - textarea (no longer included - see DOM selection below)
+        // else if (e.target.type == 'textarea') {
+        //     console.log('textarea');
+        // }
+    }
+    
+
    
 }
 
-document.getElementById('name').addEventListener('keydown',validator);
 
-var goToTop = function() {
+// get all form inputs
+const inputs = [...document.querySelectorAll('form input')];
+inputs.forEach(element=>element.addEventListener('input', validator));
+// const textarea = document.querySelector('textarea');
+// inputs.push(textarea);
+// console.log(inputs);
+
+
+
+
+
+
+
+// ______________________
+// logo scroll to top
+const goToTop = function() {
     window.scrollTo(0,0);
 }
 
-var logo = document.querySelector('.logo');
+const logo = document.querySelector('.logo');
 logo.addEventListener('click', goToTop);
